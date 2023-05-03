9ca2ce3ffbbee87b53b5a8069fc2650679add04b
Maxime Jezequel <maxime.jezequel@gmail.com>
2023-05-04 00:40:25 +0200

    Add gamepad api and configure button press

--- src/components/GamepadComponent.tsx
+++ src/components/GamepadComponent.tsx
@@ -1,36 +1,19 @@
-import React, { useEffect, useState } from "react"
-
-class MyGamepad {
-  private readonly buffer: ArrayBuffer
-  private readonly mappings: string
-
-  constructor(buffer: ArrayBuffer, options: { mappings: string }) {
-    this.buffer = buffer
-    this.mappings = options.mappings
-  }
-}
+import React, { useCallback, useEffect, useState } from "react"
 
 const GamepadComponent: React.FC = () => {
   const [gamepad, setGamepad] = useState<Gamepad | null>(null)
   const [buttonPressed, setButtonPressed] = useState<number | null>(null)
 
+  // Gamepad is connecting
   const handleGamepadConnected = (event: GamepadEvent) => {
     console.log("Gamepad connected:", event.gamepad)
     setGamepad(event.gamepad)
   }
 
+  // Gamepad is disconnecting
   const handleGamepadDisconnected = (event: GamepadEvent) => {
     console.log("Gamepad disconnected:", event.gamepad)
     setGamepad(null)
-  }
-
-  const handleGamepadButtonPress = (event: GamepadEvent) => {
-    console.log("Gamepad button pressed")
-    const buttonIndex = event.gamepad.buttons.findIndex(
-      (button) => button.pressed
-    )
-    console.log("Button pressed:", buttonIndex)
-    setButtonPressed(buttonIndex)
   }
 
   async function connectGamepad() {
@@ -48,90 +31,70 @@ const GamepadComponent: React.FC = () => {
 
       // Connect to the device
       const gatt = await device.gatt?.connect()
-
       // Get the Gamepad service
       const service = await gatt?.getPrimaryService("abxy_gamepad")
-
       // Get the Gamepad characteristic
       const characteristic = await service?.getCharacteristic(
         "abxy_gamepad_state"
       )
-
       // Create a new Gamepad object from the characteristic value
       const value = await characteristic?.readValue()
-
       if (!value) {
         console.error("Failed to read value from characteristic")
         return
       }
-
       // Create a new Gamepad object from the characteristic value
-
-      setGamepad(
-        new MyGamepad(value.buffer, {
-          mappings: "standard",
-        }) as unknown as Gamepad
-      )
+      setGamepad(new Gamepad())
     } catch (error) {
       console.error("Bluetooth connection failed", error)
     }
   }
 
+  const handleGamepadButtonPress = useCallback(
+    (event: any, setButtonPressed: Function) => {
+      console.log("Gamepad button pressed")
+      const buttonIndex = event.gamepad.buttons.findIndex(
+        (button: any) => button.pressed
+      )
+      console.log("Button pressed:", buttonIndex)
+      setButtonPressed(buttonIndex)
+    },
+    []
+  )
+
   useEffect(() => {
     window.addEventListener("gamepadconnected", handleGamepadConnected)
     window.addEventListener("gamepaddisconnected", handleGamepadDisconnected)
     requestAnimationFrame(updateGamepad)
+
+    return () => {
+      window.removeEventListener("gamepadconnected", handleGamepadConnected)
+      window.removeEventListener(
+        "gamepaddisconnected",
+        handleGamepadDisconnected
+      )
+    }
   }, [])
 
   const updateGamepad = () => {
     if (gamepad) {
-      const updatedGamepad = navigator.getGamepads()[gamepad.index]
-      if (updatedGamepad) {
-        updatedGamepad.buttons.forEach((button, index) => {
-          if (button.pressed !== gamepad.buttons[index].pressed) {
-            handleGamepadButtonPress({
-              gamepad: updatedGamepad,
-              bubbles: false,
-              cancelBubble: false,
-              cancelable: false,
-              composed: false,
-              currentTarget: null,
-              defaultPrevented: false,
-              eventPhase: 0,
-              isTrusted: false,
-              returnValue: false,
-              srcElement: null,
-              target: null,
-              timeStamp: 0,
-              type: "",
-              composedPath: function (): EventTarget[] {
-                throw new Error("Function not implemented.")
-              },
-              initEvent: function (
-                type: string,
-                bubbles?: boolean | undefined,
-                cancelable?: boolean | undefined
-              ): void {
-                throw new Error("Function not implemented.")
-              },
-              preventDefault: function (): void {
-                throw new Error("Function not implemented.")
-              },
-              stopImmediatePropagation: function (): void {
-                throw new Error("Function not implemented.")
-              },
-              stopPropagation: function (): void {
-                throw new Error("Function not implemented.")
-              },
-              NONE: 0,
-              CAPTURING_PHASE: 1,
-              AT_TARGET: 2,
-              BUBBLING_PHASE: 3,
-            })
-          }
-        })
-        setGamepad(updatedGamepad)
-      }
+      setGamepad((prevGamepad) => {
+        const updatedGamepad =
+          navigator.getGamepads()[prevGamepad?.index ?? gamepad?.index ?? 0]
+        if (updatedGamepad) {
+          updatedGamepad.buttons.forEach((button, index) => {
+            if (button.pressed !== prevGamepad?.buttons[index].pressed) {
+              handleGamepadButtonPress(
+                {
+                  gamepad: updatedGamepad,
+                },
+                () => setButtonPressed(index)
+              )
+            }
+          })
+        }
+        return updatedGamepad
+      })
     }
     requestAnimationFrame(updateGamepad)
   }
@@ -139,7 +102,10 @@ const GamepadComponent: React.FC = () => {
   return (
     <div>
       {gamepad ? (
-        <p>Gamepad connected: {gamepad.id}</p>
+        <>
+          <p>Gamepad connected: {gamepad.id}</p>
+          <p>Gamepad index: {gamepad.index}</p>
+        </>
       ) : (
         <button onClick={() => connectGamepad()}>Connect to gamepad</button>
       )}
